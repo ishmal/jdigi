ea8f381518e801c4f09d2453da2de0d8201d7c4b
Jamison, Robert <Bob.Jamison@pearson.com>
2014-06-11 14:11:12 -0500

    New file for experimentation

---
+++ src/costas.js
@@ -0,0 +1,84 @@
+/**
+ * Jdigi
+ *
+ * Copyright 2014, Bob Jamison
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var Complex = require("./math").Complex;
+
+var cossinTable = (function() {
+
+    var twopi = Math.PI * 2.0;
+    var two16 = 65536;
+    var delta = twopi / two16;
+    
+    var xs = new Array(two16);
+    
+    for (var idx = 0 ; idx < two16 ; idx++) {
+        var angle = delta * idx;
+        xs[idx] = { cos: Math.cos(angle), sin: Math.sin(angle) }; 
+    }
+    return xs;  
+})();
+
+
+function LowPassIIR(cutoff, sampleRate) {
+    var b = Math.exp(-2.0 * Math.PI * cutoff/sampleRate);
+    var a = 1.0 - b;
+    var z1 = 0.0;
+    
+    this.update = function(v) {
+        z1 = v * a + z1 * b;
+        return z1;
+    };
+
+}
+
+
+/**
+ * A sine generator with a 32-bit accumulator and a 16-bit
+ * lookup table.  Much faster than Math.whatever
+ */
+function Costas(frequency, sampleRate)
+{
+    var freq = 0|0;
+    function setFrequency(frequency) {
+        freq  = (4294967296.0 * frequency / sampleRate)|0;
+		return freq;
+    }
+    this.setFrequency = setFrequency;
+    setFrequency(frequency);
+    
+    var phase = 0|0;
+    var table = cossinTable;
+    
+    this.next = function() {
+        phase = (phase + freq) & 0xffffffff;
+        return table[(phase >> 16) & 0xffff];
+    };
+    
+    this.mixNext = function (v) {
+        phase = (phase + freq) & 0xffffffff;
+        var cs = table[(phase >> 16) & 0xffff];
+        return new Complex(v*cs.cos, v*cs.sin);
+    };
+        
+}
+
+module.exports.Costas = Costas;
+
+
+
