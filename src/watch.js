32b5ef61a6b1b82d0d83e7064fdb1b6b15f96908
ishmal <ishmalius@gmail.com>
2014-07-02 00:43:58 -0500

    Fixed Crc

--- src/watch.js
+++ src/watch.js
@@ -29,6 +29,9 @@ function Watcher(par) {
     var suffix = "(F[A-Z]{3}|[A-Z]{1,3})";  //note:  Fxxx is australian
     var call = prefix + digits + suffix;
     var spot = "[^a-zA-Z0-9]de\\s+(" + call + ")[^a-zA-Z0-9]";
+    //var spot2 = "\\s+(" + call + ")\\s+[Kk]\\s";
+    //var spot3 = "\\s+(" + call + ")\\s+[Cc][Qq]";
+    //var spot = spot1 + "|" + spot2 + "|" + spot3;
     
     var buf = "";
     var calls = {};
