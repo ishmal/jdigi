8eee9f21f13ffd003f4b280466f4a6816dbf4637
ishmal <ishmalius@gmail.com>
2014-07-07 08:02:18 -0500

    Start testing output audio

---
+++ test/testaudio.js
@@ -0,0 +1,12 @@
+
+
+var assert = require("assert");
+var AudioOutput = require("../src/audio").AudioOutput;
+
+describe('Audio Tests', function(){
+  describe('AudioOutput', function(){
+    it('Interpolation should work correctly', function(){
+
+    });
+  });
+});
