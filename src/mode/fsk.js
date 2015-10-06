505ae60cf415b7f0d0674238d17efc465fdde9f0
Bob <Bob.Jamison@pearson.com>
2015-10-06 16:01:45 -0500

    Update copyright.

--- src/mode/fsk.js
+++ src/mode/fsk.js
@@ -1,7 +1,7 @@
 /**
  * Jdigi
  *
- * Copyright 2014, Bob Jamison
+ * Copyright 2015, Bob Jamison
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
--- src/watch.js
+++ src/watch.js
@@ -1,7 +1,7 @@
 /**
  * Jdigi
  *
- * Copyright 2014, Bob Jamison
+ * Copyright 2015, Bob Jamison
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -20,65 +20,64 @@
 /**
  * @param par Digi
  */
-function Watcher(par) {
-    //This regex's groups are prefix, digit, suffix
-    var prefix = "([A-Z]{1,2}|[0-9][A-Z]|[A-Z][0-9])";
-    var digits = "([0-9])";
-    var suffix = "(F[A-Z]{3}|[A-Z]{1,3})";  //note:  Fxxx is australian
-    var call = prefix + digits + suffix;
-    var spot = "[^a-z0-9](?:de|cq)\\s+(" + call + ")[^a-z0-9]";
-    //var spot2 = "\\s+(" + call + ")\\s+[Kk]\\s";
-    //var spot3 = "\\s+(" + call + ")\\s+[Cc][Qq]";
-    //var spot = spot1 + "|" + spot2 + "|" + spot3;
+class Watcher {
 
-    var buf = "";
-    var calls = {};
+    constructor(par) {
+        this.par = par;
 
-    var useQrz = false;
-    this.setUseQrz = function (v) {
-        useQrz = v;
-    };
-    this.getUseQrz = function () {
-        return useQrz;
-    };
+        //This regex's groups are prefix, digit, suffix
+        this.prefix = "([A-Z]{1,2}|[0-9][A-Z]|[A-Z][0-9])";
+        this.digits = "([0-9])";
+        this.suffix = "(F[A-Z]{3}|[A-Z]{1,3})";  //note:  Fxxx is australian
+        this.call = prefix + digits + suffix;
+        this.spot = "[^a-z0-9](?:de|cq)\\s+(" + call + ")[^a-z0-9]";
+        //var spot2 = "\\s+(" + call + ")\\s+[Kk]\\s";
+        //var spot3 = "\\s+(" + call + ")\\s+[Cc][Qq]";
+        //var spot = spot1 + "|" + spot2 + "|" + spot3;
+        this.buf = "";
+        this.calls = {};
 
-    function showQrz(call) {
-        if (useQrz)
+        this.useQrz = false;
+
+        this.timeout = 300000; //5 mins
+    }
+
+
+    showQrz(call) {
+        if (this.useQrz)
             window.open("http://qrz.com/db/" + call,
                 "qrzquery", "menubar=true,toolbar=true");
     }
 
-    function announce(call) {
-        var msg = call.ts.toUTCString() + " : " + call.call + " : " +
+    announce(call) {
+        let msg = call.ts.toUTCString() + " : " + call.call + " : " +
             call.freq + " : " + call.mode + "/" + call.rate;
-        par.status(msg);
-        showQrz(call.call);
+        this.par.status(msg);
+        this.showQrz(call.call);
     }
 
-    function check(call) {
-        var csn = call.call;
-        if (csn in calls) {
-            var curr = calls[csn];
-            var diff = call.ts.getTime() - curr.ts.getTime();
-            if (diff > 300000) { //5 mins
+    check(call) {
+        let csn = call.call;
+        if (csn in this.calls) {
+            let curr = this.calls[csn];
+            let diff = call.ts.getTime() - curr.ts.getTime();
+            if (diff > this.timeout) {
                 curr.ts = call.ts;
-                announce(call);
+                this.announce(call);
             }
         } else {
-            calls[csn] = call;
-            announce(call);
+            this.calls[csn] = call;
+            this.announce(call);
         }
-
     }
 
-    function searchBuffer(str) {
-        var rgx = new RegExp(spot, "ig");
-        var calls = {};
-        for (var res = rgx.exec(str); res !== null; res = rgx.exec(str)) {
-            var mode = par.getMode();
-            var name = mode.properties.name;
-            var rate = mode.getRate();
-            var call = {
+    searchBuffer(str) {
+        let rgx = new RegExp(spot, "ig");
+        for (let res = rgx.exec(str); res !== null; res = rgx.exec(str)) {
+            let mode = this.par.getMode();
+            let name = mode.properties.name;
+            let rate = mode.getRate();
+            let call = {
                 call: res[1].toLowerCase(),
                 prefix: res[2].toLowerCase(),
                 digit: res[3],
@@ -88,20 +87,18 @@ function Watcher(par) {
                 rate: rate,
                 ts: new Date() //timestamp
             };
-            check(call);
+            this.check(call);
         }
-        return calls;
     }
 
-    this.update = function (str) {
-        buf += str;
-        searchBuffer(buf);
-        var len = buf.length;
+    update(str) {
+        this.buf += str;
+        this.searchBuffer(this.buf);
+        let len = this.buf.length;
         if (len > 30) {
-            buf = buf.substring(20, len);
+            this.buf = this.buf.substring(20, len);
         }
-
-    };
+    }
 }
 
 export {Watcher};
