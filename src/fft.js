8d91201e52891f123dcf1b5194123f911107c562
ishmal <ishmalius@gmail.com>
2015-10-05 22:24:50 -0500

    tweak

--- src/fft.js
+++ src/fft.js
@@ -26,17 +26,17 @@ import {Window} from "./window";
  */
 function FFT(N) {
 
-    var N2 = N/2;
-    var power = (Math.log(N) / Math.LN2) | 0;
-    var nrStages = power;
+    let N2 = N/2;
+    let power = (Math.log(N) / Math.LN2) | 0;
+    let nrStages = power;
     //todo: validate power-of-2, throw IAE if not
 
     function createBitReversedIndices(n) {
-        var xs = new Array(n);
-        for (var i=0 ; i < n ; i++) {
-           var np = n;
-           var index = i;
-           var bitreversed = 0;
+        let xs = new Array(n);
+        for (let i=0 ; i < n ; i++) {
+          let np = n;
+           let index = i;
+           let bitreversed = 0;
            while (np > 1) {
                bitreversed <<= 1;
                bitreversed += index & 1;
@@ -47,27 +47,27 @@ function FFT(N) {
         }
         return xs;
     }
-    var bitReversedIndices = createBitReversedIndices(N);
+    let bitReversedIndices = createBitReversedIndices(N);
 
     /**
      * This piece does not need to be fast, just correct
      */
     function generateStageData() {
-        var xs = [];
-        var span  = 1;
-        var wspan = N2;
-        var ninv = 1 / N;
-        for (var stage=0 ; stage < nrStages ; stage++, span <<= 1, wspan >>= 1) {
-            var stageData  = [];
+        let xs = [];
+        let span  = 1;
+        let wspan = N2;
+        let ninv = 1 / N;
+        for (let stage=0 ; stage < nrStages ; stage++, span <<= 1, wspan >>= 1) {
+            let stageData  = [];
             for (submatrix=0; submatrix<N2/span; submatrix++) {
-                var np = submatrix * span * 2;
-                var ni = np;
+                let np = submatrix * span * 2;
+                let ni = np;
                 for (node=0; node<span ; node++) {
-                   var l = ni;
-                   var r = ni + span;
-                   var idx = node * wspan;
-                   var wr = Math.cos(Math.PI*2.0*node*wspan * ninv);
-                   var wi = Math.sin(Math.PI*2.0*node*wspan * ninv);
+                   let l = ni;
+                   let r = ni + span;
+                   let idx = node * wspan;
+                   let wr = Math.cos(Math.PI*2.0*node*wspan * ninv);
+                   let wi = Math.sin(Math.PI*2.0*node*wspan * ninv);
                    stageData[stageData.length] = {l:l,r:r,wr:wr,wi:wi,idx:idx};
                    ni++;
                 }
@@ -76,37 +76,37 @@ function FFT(N) {
         }
         return xs;
     }
-    var stages = generateStageData();
+    let stages = generateStageData();
 
 
     function apply(input) {
 
         //local refs
-        var n2     = N2;
-        var nrStgs = nrStages;
-        var stgs   = stages;
+        let n2     = N2;
+        let nrStgs = nrStages;
+        let stgs   = stages;
 
-        var xr = new Array(N);
-        var xi = new Array(N);
-        for (var idx = 0 ; idx<N ; idx++) {
+        let xr = new Array(N);
+        let xi = new Array(N);
+        for (let idx = 0 ; idx<N ; idx++) {
             //todo:  apply Hann window here
-            var bri = bitReversedIndices[idx];
+            let bri = bitReversedIndices[idx];
             xr[idx] = input[bri];
             xi[idx] = 0;
         }
 
-        for (var stage=0 ; stage<nrStgs ; stage++) {
-            var stageData = stgs[stage];
-            for (var i = 0; i < n2; i++) {
-                var parms  = stageData[i];
-                var wr     = parms.wr;
-                var wi     = parms.wi;
-                var left   = parms.l;
-                var right  = parms.r;
-                var leftr  = xr[left];
-                var lefti  = xi[left];
-                var rightr = wr * xr[right] - wi * xi[right];
-                var righti = wi * xr[right] + wr * xi[right];
+        for (let stage=0 ; stage<nrStgs ; stage++) {
+            let stageData = stgs[stage];
+            for (let i = 0; i < n2; i++) {
+                let parms  = stageData[i];
+                let wr     = parms.wr;
+                let wi     = parms.wi;
+                let left   = parms.l;
+                let right  = parms.r;
+                let leftr  = xr[left];
+                let lefti  = xi[left];
+                let rightr = wr * xr[right] - wi * xi[right];
+                let righti = wi * xr[right] + wr * xi[right];
                 xr[left]   = leftr + rightr;
                 xi[left]   = lefti + righti;
                 xr[right]  = leftr - rightr;
@@ -119,15 +119,15 @@ function FFT(N) {
 
     function powerSpectrum(input) {
 
-        var x  = apply(input);
-        var xr = x.r;
-        var xi = x.i;
-        var len  = N2;
+        let x  = apply(input);
+        let xr = x.r;
+        let xi = x.i;
+        let len  = N2;
 
-        var ps = new Array(len);
-        for (var j=0 ; j<len ; j++) {
-            var r = xr[j];
-            var i = xi[j];
+        let ps = new Array(len);
+        for (let j=0 ; j<len ; j++) {
+            let r = xr[j];
+            let i = xi[j];
             ps[j] = r*r + i*i;
         }
         return ps;
@@ -145,15 +145,15 @@ function FFT(N) {
  */
 function FFTSR(N) {
 
-    var power = (Math.log(N) / Math.LN2) | 0;
-    var N2 = N >> 1;
+    let power = (Math.log(N) / Math.LN2) | 0;
+    let N2 = N >> 1;
 
     function generateBitReversedIndices(n) {
-        var xs = new Array(n);
-        for (var i=0 ; i < n ; i++) {
-           var np = n;
-           var index = i;
-           var bitreversed = 0;
+        let xs = new Array(n);
+        for (let i=0 ; i < n ; i++) {
+           let np = n;
+           let index = i;
+           let bitreversed = 0;
            while (np > 1) {
                bitreversed <<= 1;
                bitreversed += index & 1;
@@ -164,18 +164,18 @@ function FFTSR(N) {
         }
         return xs;
     }
-    var bitReversedIndices = generateBitReversedIndices(N);
+    let bitReversedIndices = generateBitReversedIndices(N);
 
     //let's pre-generate anything we can
     function generateStageData(pwr) {
-        var xs = [];
-        var n2 = N;// == n>>(k-1) == n, n/2, n/4, ..., 4
-        var n4 = n2>>2; // == n/4, n/8, ..., 1
-        for (var k=1 ; k<pwr ; k++, n2>>=1, n4>>=1) {
-            var stage = [];
-            var e = 2.0 * Math.PI / n2;
-            for (var j=1; j<n4; j++) {
-                var a = j * e;
+        let xs = [];
+        let n2 = N;// == n>>(k-1) == n, n/2, n/4, ..., 4
+        let n4 = n2>>2; // == n/4, n/8, ..., 1
+        for (let k=1 ; k<pwr ; k++, n2>>=1, n4>>=1) {
+            let stage = [];
+            let e = 2.0 * Math.PI / n2;
+            for (let j=1; j<n4; j++) {
+                let a = j * e;
                 stage[stage.length] = {
                     wr1: Math.cos(a),   wi1: Math.sin(a),
                     wr3: Math.cos(3*a), wi3: Math.sin(3*a)
@@ -186,12 +186,12 @@ function FFTSR(N) {
         return xs;
     }
 
-    var stages = generateStageData(power);
-    var W = Window.hann(N);
-    var xr = new Float32Array(N);
-    var xi = new Float32Array(N);
-    var ZEROES = new Float32Array(N);
-    
+    let stages = generateStageData(power);
+    let W = Window.hann(N);
+    let xr = new Float32Array(N);
+    let xi = new Float32Array(N);
+    let ZEROES = new Float32Array(N);
+
 
     /**
      * Real samples
@@ -201,33 +201,33 @@ function FFTSR(N) {
         xi.set(ZEROES);
         compute();
     }
-    
+
     /**
      * Complex samples
      */
     function applyX(input) {
-        for (var idx=0 ; idx<N ; idx++) {
-            var cx = input[idx];
+        for (let idx=0 ; idx<N ; idx++) {
+            let cx = input[idx];
             xr[idx] = cx.r; // * W[idx];
             xi[idx] = cx.i;
         }
         compute();
     }
-    
-    
-    function compute() {
-        var ix, id, i0, i1, i2, i3;
-        var j,k;
-        var tr, ti, tr0, ti0, tr1, ti1;
-        var n2, n4;
 
-        var stageidx = 0;
+
+    function compute() {
+        let ix, id, i0, i1, i2, i3;
+        let j,k;
+        let tr, ti, tr0, ti0, tr1, ti1;
+        let n2, n4;
+
+        let stageidx = 0;
 
         n2 = N;  // == n>>(k-1) == n, n/2, n/4, ..., 4
         n4 = n2>>2; // == n/4, n/8, ..., 1
         for (k=1; k<power; k++, n2>>=1, n4>>=1) {
 
-            var stage = stages[stageidx++];
+            let stage = stages[stageidx++];
 
             id = (n2<<1);
             for (ix=0 ; ix<N ; ix=(id<<1)-n2, id <<= 2)  { //ix=j=0
@@ -268,15 +268,15 @@ function FFTSR(N) {
             }
 
 
-        var dataindex = 0;
+        let dataindex = 0;
 
         for (j=1; j<n4; j++) {
 
-            var data = stage[dataindex++];
-            var wr1 = data.wr1;
-            var wi1 = data.wi1;
-            var wr3 = data.wr3;
-            var wi3 = data.wi3;
+            let data = stage[dataindex++];
+            let wr1 = data.wr1;
+            let wi1 = data.wi1;
+            let wr3 = data.wr3;
+            let wi3 = data.wi3;
 
             id = (n2<<1);
             for (ix=j ; ix<N ; ix = (id<<1)-n2+j, id <<= 2) {
@@ -335,12 +335,12 @@ function FFTSR(N) {
 
 
     function computePowerSpectrum(ps) {
-        var len = N2;
-        var indices = bitReversedIndices;
-        for (var j=0 ; j<len ; j++) {
-            var bri = indices[j];
-            var r = xr[bri];
-            var i = xi[bri];
+        let len = N2;
+        let indices = bitReversedIndices;
+        for (let j=0 ; j<len ; j++) {
+            let bri = indices[j];
+            let r = xr[bri];
+            let i = xi[bri];
             ps[j] = r*r + i*i;
         }
     }
@@ -366,18 +366,18 @@ function FFTSR(N) {
 function SimpleGoertzel(frequency, binWidth, sampleRate) {
 
     //how many bins would there be for this binWidth? Integer
-    var N     = (sampleRate / binWidth) | 0;
+    let N     = (sampleRate / binWidth) | 0;
     //which bin out of N are we? Must be an integer.
-    var bin   = (0.5 + frequency / sampleRate * N) | 0; 
-    var w     = 2.0 * Math.PI / N * bin;  //omega
-    var wr    = Math.cos(w);
-    var wr2   = 2.0 * wr;
-    var wi    = Math.sin(w);
-    var pr1=0, pr2=0, pi1=0, pi2=0;
-    var damping = 0.999;
-    
+    let bin   = (0.5 + frequency / sampleRate * N) | 0;
+    let w     = 2.0 * Math.PI / N * bin;  //omega
+    let wr    = Math.cos(w);
+    let wr2   = 2.0 * wr;
+    let wi    = Math.sin(w);
+    let pr1=0, pr2=0, pi1=0, pi2=0;
+    let damping = 0.999;
+
     this.N = N;
-    
+
     this.reset = function() {
         pr1=0; pr2=0; pi1=0; pi2=0;
     };
@@ -389,18 +389,18 @@ function SimpleGoertzel(frequency, binWidth, sampleRate) {
 
     //use this if computed with updateX()
     this.X = function() {
-        var realr = wr * pr1 - pr2;
-        var reali = wi * pr1;
-        var imagr = wr * pi1 - p12;
-        var imagi = wi * pi1;
-        return {r:realr-imagi, i:reali+imagr}; 
+        let realr = wr * pr1 - pr2;
+        let reali = wi * pr1;
+        let imagr = wr * pi1 - p12;
+        let imagi = wi * pi1;
+        return {r:realr-imagi, i:reali+imagr};
     };
 
     //faster for power spectrum
     this.mag = function() {
         return pr1 * pr1 + pr2 * pr2;
     };
-    
+
     //correct
     this.mag2 = function() {
         return pr1 * pr1 + pr2 * pr2 - wr2 * pr1 * pr2;
@@ -412,16 +412,16 @@ function SimpleGoertzel(frequency, binWidth, sampleRate) {
     };
 
     this.update = function(point) {
-        var r = point + (pr1 * wr2 - pr2);
+        let r = point + (pr1 * wr2 - pr2);
         pr2 = pr1 - point;
         pr1 = r;
     };
 
     this.updateX = function(point) {
-        var r = point.r + (pr1 * wr2 - pr2);
+        let r = point.r + (pr1 * wr2 - pr2);
         pr2 = pr1 - point.r;
         pr1  = r * damping;
-        var i = point.i + (pi1 * wr2 - pi2);
+        let i = point.i + (pi1 * wr2 - pi2);
         pi2 = pi1 - point.i;
         pi1  = i * damping;
     };
@@ -433,5 +433,3 @@ function SimpleGoertzel(frequency, binWidth, sampleRate) {
 
 
 export {FFT,FFTSR,SimpleGoertzel};
-
-
